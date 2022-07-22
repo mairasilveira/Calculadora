3942089673cccff23118ad2c63eb90cb57e68bf1
Maira Silveira <silveirasmaira@gmail.com>
2022-07-21 22:01:37 -0300

    DOM

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,19 +1,30 @@
 class CalcController {
 
-    constructor() {
+    constructor(){
         this._displayCalc = "0";
-        this._currentDate = 
+        this._currentDate;
+        this.initialize();
     }
 
-    get displayCalc() {
+    initialize(){
+    let displayCalcEl = document.querySelector("#display");
+    let dateEl = document.querySelector("#data");
+    let timeEl = document.querySelector("#hora");
+
+    displayCalcEl.innerHTML = "4567";
+    dateEl.innerHTML = "01/05/2022";
+    timeEl.innerHTML = "11:00";
+    }
+
+    get displayCalc(){
         return this._displayCalc;
     }
 
-    set displayCalc(valor) {
+    set displayCalc(valor){
         this._displayCalc = valor;
     }
 
-    get dataAtual() {
+    get dataAtual(){
         return this._currentDate;
     }
 

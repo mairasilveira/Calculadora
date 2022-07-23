2141706d575f796c71e1a731dcc69f83778d3938
Maira Silveira <silveirasmaira@gmail.com>
2022-07-23 18:46:19 -0300

    eventos usando split

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,64 +1,81 @@
 class CalcController {
+  constructor() {
+    this._locale = "pt-BR";
+    this._displayCalcEl = document.querySelector("#display");
+    this._dateEl = document.querySelector("#data");
+    this._timeEl = document.querySelector("#hora");
+    this._currentDate;
+    this.initialize();
+    this.initButtonsEvents();
+  }
 
-    constructor() {
-        this._locale = 'pt-BR';
-        this._displayCalcEl = document.querySelector("#display");
-        this._dateEl = document.querySelector("#data");
-        this._timeEl = document.querySelector("#hora");
-        this._currentDate;
-        this.initialize();
-    }
+  initialize() {
+    this.setDisplayDateTime();
 
-    initialize() {
+    setInterval(() => {
+      this.setDisplayDateTime();
+    }, 1000); //cada 1000 milisegundos a função roda dnv, hora vai atualizar a cada segundo
+}
 
-        this.setDisplayDateTime()
-
-       setInterval(()=>{
-
-        this.setDisplayDateTime();
-
-       }, 1000); //cada 1000 milisegundos a função roda dnv, hora vai atualizar a cada segundo
-    }
-
-    setDisplayDateTime() {
-        this.displayDate = this.currentDate.toLocaleDateString(this._locale, { //esse currentDate é o new date la embaixo
-            day: "2-digit",
-            month: "long",
-            year: "numeric"
+    addEventListenerAll(element, events, fn){
+        events.split(' ').forEach(event => {
+            element.addEventListener(event, fn, false);
         });
-        this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
     }
+  
 
-    get displayTime() {
-        return this._timeEl.innerHTML;
-    }
+  initButtonsEvents() {
+    let buttons = document.querySelectorAll("#buttons > g, #parts > g"); //> significa que g é filho de #buttons
+    buttons.forEach((btn, index) => {
+      this.addEventListenerAll(btn, "click drag", e => {
+        console.log(btn.className.baseVal.replace("btn-", ""));
+      });
 
-    set displayTime(value) {
-        this._timeEl.innerHTML = value;
-    }
+      this.addEventListenerAll(btn, "mouseover mouseup mousedown", e => {
+        btn.style.cursor = "pointer";
+      });
+    });
+  }
 
-    get displayDate() {
-        return this._dateEl.innerHTML;
-    }
+  setDisplayDateTime() {
+    this.displayDate = this.currentDate.toLocaleDateString(this._locale, {
+      //esse currentDate é o new date la embaixo
+      day: "2-digit",
+      month: "long",
+      year: "numeric",
+    });
+    this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+  }
 
-    set displayDate(value) {
-        this._dateEl.innerHTML = value;
-    }
+  get displayTime() {
+    return this._timeEl.innerHTML;
+  }
 
-    get displayCalc() {
-        return this._displayCalcEl.innerHTML;
-    }
+  set displayTime(value) {
+    this._timeEl.innerHTML = value;
+  }
 
-    set displayCalc(value) {
-        this._displayCalcEl.innerHTML = value;
-    }
+  get displayDate() {
+    return this._dateEl.innerHTML;
+  }
 
-    get currentDate() {
-        return new Date();
-    }
+  set displayDate(value) {
+    this._dateEl.innerHTML = value;
+  }
 
-    set currentDate(value) {
-        this._currentDate = value;
-    }
+  get displayCalc() {
+    return this._displayCalcEl.innerHTML;
+  }
 
+  set displayCalc(value) {
+    this._displayCalcEl.innerHTML = value;
+  }
+
+  get currentDate() {
+    return new Date();
+  }
+
+  set currentDate(value) {
+    this._currentDate = value;
+  }
 }
